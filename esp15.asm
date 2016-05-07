ad59fa89fbb107e65202bf27da9a02a4293605da
Canteri Marco <canterimarco@gmail.com>
2016-05-07 12:42:06 +0200

    aggiunta regressione laser arancione

--- esp15.asm
+++ esp15.asm
@@ -30,8 +30,6 @@ red_laser:
         dq 2127.1,1031.1,662.1,269.1,44.6,15.1,6.25, 1.17
 orange_laser:
         dq 1388.6,1111.6,879.6,656.6,336.6,196.6,136.6,90.6
-log_2e:
-        dq 1.4425454561053044
 log_e2:
         dq 0.6931471805599453
 eight:
@@ -130,11 +128,11 @@ print_conc:
 
 ; calcoli con la fpu
         ;printa il quarto messaggio
-        mov     rax, 1                  ; system call write
-        mov     rdi, 1                  ; standard output
-        mov     rsi, msg4         
-        mov     rdx, 22                 ; byte della stringa
-        syscall                         ; system call
+        ;mov     rax, 1                  ; system call write
+        ;mov     rdi, 1                  ; standard output
+        ;mov     rsi, msg4         
+        ;mov     rdx, 22                 ; byte della stringa
+        ;syscall                         ; system call
 
         xor     ecx,ecx
 log_loop_red:
@@ -152,7 +150,7 @@ log_loop_red:
         mov     rdi, fmt                ; formato
         movq    xmm0, qword [red_laser+ecx*8]; numero
         mov     rax, 1                  ;abbiamo usato un reistro xmm
-        call    printf                  
+        ;call    printf                  
 
         pop     rcx                     ; ripristiniamo il registro
         pop     rax                     ; ripristiniamo il registro
@@ -193,7 +191,7 @@ log_loop_orange:
         cmp     ecx,8                   ; loop su 8 elementi
         jb     log_loop_orange            ; jump se ecx<8
 
-;Linear Fitting!!!!!!
+;Linear Fitting!!!!!!  RED LASER
 
         movsd xmm1, [eight]                  ;sum(w)
 
@@ -249,6 +247,91 @@ sum_x_y:
         inc ecx
         cmp ecx,8
         jb sum_x_y
+
+        ;xmm1 = sum(w),xmm0 = sum(x^2), xmm3 = sum(x), xmm4 = nabla xmm5 = sum(y) xmm6 = sum(xy)
+
+        movq xmm7,xmm0
+        mulsd xmm7,xmm5
+        movq xmm8,xmm3
+        mulsd xmm8,xmm6
+        subsd xmm7,xmm8
+        divsd xmm7,xmm4
+
+        ; xmm7 = a! ;xmm1 = sum(w),xmm0 = sum(x^2), xmm3 = sum(x), xmm4 = nabla xmm5 = sum(y) xmm6 = sum(xy)
+
+        movq xmm8,xmm1
+        mulsd xmm8,xmm6
+        movq xmm9,xmm5
+        mulsd xmm9,xmm3
+        subsd xmm8,xmm9
+        divsd xmm8,xmm4
+
+        ;xmm8 = b!  xmm7 = a! ;xmm1 = sum(w),xmm0 = sum(x^2), xmm3 = sum(x), xmm4 = nabla xmm5 = sum(y) xmm6 = sum(xy)
+
+        ;stampa il risultato
+        mov     rdi, stampa_a_b               
+        movq    xmm0,xmm7
+        movq    xmm1,xmm8
+        mov     rax, 2                  ;abbiamo usato due registri xmm
+        call    printf         
+
+
+;Linear Fitting!!!!!! ORANGE LASER
+
+        movsd xmm1, [eight]                  ;sum(w)
+
+        xor ecx,ecx
+        movsd xmm0,[zero]                   ;somma parziale
+
+sum_x_2_orange:
+        movsd xmm2,[concentrazioni+ecx*8]
+        mulsd xmm2,xmm2                 ;x^2  
+        addsd xmm0,xmm2                 ;xxm0+=x^2
+
+        inc ecx
+        cmp ecx,8
+        jb sum_x_2_orange
+        ;xmm1 = sum(w),xmm0 = sum(x^2)
+        xor ecx,ecx
+        movsd xmm3,[zero]             ;somma parziale
+sum_x_orange:
+        movsd xmm2,[concentrazioni+ecx*8]
+        addsd xmm3,xmm2                 ;xxm0+=x^2
+        inc ecx
+        cmp ecx,8
+        jb sum_x_orange
+
+        ;xmm1 = sum(w),xmm0 = sum(x^2), xmm3 = sum(x)
+
+        movq xmm4,xmm0                  ;xmm4= sum(x^2) 
+        mulsd xmm4,xmm1                 ;xmm4= sum(w)*sum(x^2) 
+        movq xmm8,xmm3
+        mulsd xmm8,xmm3                 ;xmm3 = sum(x)^2
+        subsd xmm4,xmm8                 ;xmm4= sum(w)*sum(x^2) -sum(x)^2
+
+        ;xmm1 = sum(w),xmm0 = sum(x^2), xmm3 = sum(x), xmm4 = nabla
+
+        xor ecx,ecx
+        movsd xmm5,[zero]             ;somma parziale
+sum_orange:
+        movsd xmm2,[orange_laser+ecx*8]
+        addsd xmm5,xmm2
+        inc ecx
+        cmp ecx,8
+        jb sum_orange
+
+        ;xmm1 = sum(w),xmm0 = sum(x^2), xmm3 = sum(x), xmm4 = nabla xmm5 = sum(y)
+
+        xor ecx,ecx
+        movsd xmm6,[zero]
+sum_x_y_orange:
+        movsd xmm2,[orange_laser+ecx*8]
+        movsd xmm7,[concentrazioni+ecx*8]
+        mulsd xmm2,xmm7
+        addsd xmm6,xmm2
+        inc ecx
+        cmp ecx,8
+        jb sum_x_y_orange
 
         ;xmm1 = sum(w),xmm0 = sum(x^2), xmm3 = sum(x), xmm4 = nabla xmm5 = sum(y) xmm6 = sum(xy)
 
